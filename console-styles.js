c1febcdb0e16573e4ea40654cbf1c1c52f43be33
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-20 10:46:31 -0500

    Update console-styles.js

--- console-styles.js
+++ console-styles.js
@@ -27,7 +27,7 @@ Object.defineProperty(console, "style", {
 			return this.join("", style);
 		},
 		"joinStyle": function(style) {
-			return this.applyStyle(style + " " + this.data[this.data.length - 1])
+			return this.applyStyle(this.data[this.data.length - 1] + " " + style)
 		}
 	}),
 	"writable": true
