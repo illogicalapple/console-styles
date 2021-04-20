cab77e7f484426b89daeffc798d4c37f9b1fd358
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-20 09:57:10 -0500

    Create console-styles.js

---
+++ console-styles.js
@@ -0,0 +1,24 @@
+Object.defineProperty(console, "style", {
+	"value": (value, style) => ({
+		"data": [
+			"%c" + value.replaceAll("%", "\%"),
+			style
+		],
+		"join": function(value, style) {
+			let data = this.data;
+			let join = this.join;
+			let go = this.go;
+			data.push(style);
+			data[0] += "%c" + value.replaceAll("%", "\%");
+			return {
+				"data": data,
+				"join": join,
+				"go": go
+			};
+		},
+		"go": function() {
+			console.log(...this.data);
+		}
+	}),
+	"writable": true
+});
